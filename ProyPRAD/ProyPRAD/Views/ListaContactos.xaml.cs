e2d9b6e9437737070fe336d534e95f0c13506899
Gerardo <Gerardo@PCGDRR>
2023-04-13 23:21:01 -0600

    Agregar archivos de proyecto.

---
+++ ProyPRAD/ProyPRAD/Views/ListaContactos.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyPRAD.Controllers;
+using ProyPRAD.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using ProyPRAD.Models;
+
+
+namespace ProyPRAD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListaContactos : ContentPage
+    {
+        public ListaContactos()
+        {
+            InitializeComponent();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listacontactos.ItemsSource = await DateBase.ObtenerListaContactos();
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageUpdContact());
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageDelContact());
+        }
+
+        private async void listacontactos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            var Answer = await Application.Current.MainPage.DisplayAlert("Informacion", "Desea llamar al Contacto?", "Si", "No");
+            if (Answer == true)
+            {
+                try
+                {
+                    PhoneDialer.Open("88888");
+
+                }catch
+                 (System. Exception)
+                {
+                    _ = DisplayAlert("No se puede hacer llamada", "intente de nuevo", "ok");
+
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnllamar_Clikced(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
